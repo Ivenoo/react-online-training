258a5073b6e26c4e09bc03ca5403b5beb76913aa
Ivenoo <32682666+Ivenoo@users.noreply.github.com>
2019-07-22 14:52:32 +0200

    sd

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>Hello...</p>, document.getElementById('root'));
+import Events from './Events';
+
+ReactDOM.render(<Events />, document.getElementById('root'));
